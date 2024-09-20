822f966d36243df878e2aafab2d68cdde278489f
ValeriaS22 <165791800+ValeriaS22@users.noreply.github.com>
2024-09-20 07:24:30 -0500

    se crea archivo para simular la base de datos

---
+++ src/controllers/basedatos.js
@@ -0,0 +1,17 @@
+
+let gastosDos = Array(
+    {
+        id:1,
+        description: "Pasaje",
+        monto: 4400,
+        date: "2024-09-20"
+    },
+    {
+        id:2,
+        description: "Pastel",
+        monto: 5000,
+        date: "2024-09-20"
+    },
+)
+    
+// ESCRIBIR LOS ARREGLOS EN PLURAL 
